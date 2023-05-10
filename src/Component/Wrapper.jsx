0a00bd27ca2b7d3c340d2e95b9c71f03e06f47a4
emarkees <emarkees.com>
2023-05-10 17:14:24 -0600

    Add Wrapper.jsx files

---
+++ src/Component/Wrapper.jsx
@@ -0,0 +1,12 @@
+import Screen from './Screen';
+import Calculator from './Calculator';
+import '../index.css';
+
+const Wrapper = () => (
+  <div className="wrapper container">
+    <Screen />
+    <Calculator />
+  </div>
+);
+
+export default Wrapper;
