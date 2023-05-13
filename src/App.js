6ef4dcbb64bb1230d6b44187d475d10b4f852e15
emarkees <rphillipse@gmail.com>
2023-05-13 13:47:18 -0600

    UPdate index.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Calculator from './Component/Calculator';
+import Quote from './Component/Quote';
 
 function App() {
   return (
-    <Calculator />
+    <div>
+     
+      <Calculator />
+    </div>
   );
 }
 
